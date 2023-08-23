452a6824e24aad100272ad4c95b66392ad6a0b7e
thant thet <thantthetkz@gmail.com>
2023-08-23 17:27:38 +0630

    convert Languages enum as struct so can be extendable

--- Sources/LanguageManagerSwiftUI/Util/Constants/Languages.swift
+++ Sources/LanguageManagerSwiftUI/Util/Constants/Languages.swift
@@ -7,20 +7,62 @@
 
 import Foundation
 
-public enum Languages: String {
-  case ar, en, nl, ja, ko, vi, ru, sv, fr, es, pt, it, de, da, fi, nb, tr, el, id,
-       ms, th, hi, hu, pl, cs, sk, uk, hr, ca, ro, he, ur, fa, ku, arc, sl, ml, am
-  case enGB = "en-GB"
-  case enAU = "en-AU"
-  case enCA = "en-CA"
-  case enIN = "en-IN"
-  case frCA = "fr-CA"
-  case esMX = "es-MX"
-  case ptBR = "pt-BR"
-  case zhHans = "zh-Hans"
-  case zhHant = "zh-Hant"
-  case zhHK = "zh-HK"
-  case es419 = "es-419"
-  case ptPT = "pt-PT"
-  case deviceLanguage
+public struct Languages: Equatable {
+  static let ar = Languages(rawValue: "ar")
+  static let en = Languages(rawValue: "en")
+  static let nl = Languages(rawValue: "nl")
+  static let ja = Languages(rawValue: "ja")
+  static let ko = Languages(rawValue: "ko")
+  static let vi = Languages(rawValue: "vi")
+  static let ru = Languages(rawValue: "ru")
+  static let sv = Languages(rawValue: "sv")
+  static let fr = Languages(rawValue: "fr")
+  static let es = Languages(rawValue: "es")
+  static let pt = Languages(rawValue: "pt")
+  static let it = Languages(rawValue: "it")
+  static let de = Languages(rawValue: "de")
+  static let da = Languages(rawValue: "da")
+  static let fi = Languages(rawValue: "fi")
+  static let nb = Languages(rawValue: "nb")
+  static let tr = Languages(rawValue: "tr")
+  static let el = Languages(rawValue: "el")
+  static let id = Languages(rawValue: "id")
+  static let ms = Languages(rawValue: "ms")
+  static let th = Languages(rawValue: "th")
+  static let hi = Languages(rawValue: "hi")
+  static let hu = Languages(rawValue: "hu")
+  static let pl = Languages(rawValue: "pl")
+  static let cs = Languages(rawValue: "cs")
+  static let sk = Languages(rawValue: "sk")
+  static let uk = Languages(rawValue: "uk")
+  static let hr = Languages(rawValue: "hr")
+  static let ca = Languages(rawValue: "ca")
+  static let ro = Languages(rawValue: "ro")
+  static let he = Languages(rawValue: "he")
+  static let ur = Languages(rawValue: "ur")
+  static let fa = Languages(rawValue: "fa")
+  static let ku = Languages(rawValue: "ku")
+  static let arc = Languages(rawValue: "arc")
+  static let sl = Languages(rawValue: "sl")
+  static let ml = Languages(rawValue: "ml")
+  static let am = Languages(rawValue: "am")
+  static let enGB = Languages(rawValue: "en-GB")
+  static let enAU = Languages(rawValue: "en-AU")
+  static let enCA = Languages(rawValue: "en-CA")
+  static let enIN = Languages(rawValue: "en-IN")
+  static let frCA = Languages(rawValue: "fr-CA")
+  static let esMX = Languages(rawValue: "es-MX")
+  static let ptBR = Languages(rawValue: "pt-BR")
+  static let zhHans = Languages(rawValue: "zh-Hans")
+  static let zhHant = Languages(rawValue: "zh-Hant")
+  static let zhHK = Languages(rawValue: "zh-HK")
+  static let es419 = Languages(rawValue: "es-419")
+  static let ptPT = Languages(rawValue: "pt-PT")
+  static let deviceLanguage = Languages(rawValue: "")
+  
+  public let rawValue: String
+  
+  public init(rawValue: String) {
+    self.rawValue = rawValue
+  }
 }
